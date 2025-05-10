60559c8ad2bfcccdca2c40f47274b438968544d0
Tianya Chen <tianyakc@gmail.com>
2025-05-10 16:45:45 -0400

    added the piece classes and their move directions

---
+++ src/app/chess-logic/models.ts
@@ -0,0 +1,24 @@
+export enum Color {
+  White,
+  Black
+}
+
+export type Coords = {
+  x: number,
+  y: number,
+}
+
+export enum FENChar{
+  WhitePawn="P",
+  WhiteKnight="N",
+  WhiteBishop="B",
+  WhiteRook="R",
+  WhiteQueen="Q",
+  WhiteKing="K",
+  BlackPawn="p",
+  BlackKnight="n",
+  BlackBishop="n",
+  BlackRook="r",
+  BlackQueen="q",
+  BlackKing="k",
+}
