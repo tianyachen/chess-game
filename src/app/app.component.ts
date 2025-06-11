fb40d687f82daa1b89dc0af56d3ca385a8d728b1
Tianya Chen <tianyakc@gmail.com>
2025-06-11 13:53:14 -0400

    added the chess board component to the app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { ChessBoardComponent } from "./modules/chess-board/chess-board.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ChessBoardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
