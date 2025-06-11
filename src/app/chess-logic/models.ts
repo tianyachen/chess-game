f1b1b7510fb8d3f461db65812ab20de0e5ca9a9b
Tianya Chen <tianyakc@gmail.com>
2025-06-11 13:51:46 -0400

    added the chess board models for pieces, player color etc.

--- src/app/chess-logic/models.ts
+++ src/app/chess-logic/models.ts
@@ -1,3 +1,5 @@
+import { Piece } from "./pieces/piece";
+
 export enum Color {
   White,
   Black
@@ -17,8 +19,46 @@ export enum FENChar{
   WhiteKing="K",
   BlackPawn="p",
   BlackKnight="n",
-  BlackBishop="n",
+  BlackBishop="b",
   BlackRook="r",
   BlackQueen="q",
   BlackKing="k",
 }
+
+export const pieceImagePaths: Readonly<Record<FENChar, string>> = {
+  [FENChar.WhitePawn]: "assets/pieces/white pawn.svg",
+  [FENChar.WhiteKnight]: "assets/pieces/white knight.svg",
+  [FENChar.WhiteBishop]: "assets/pieces/white bishop.svg",
+  [FENChar.WhiteRook]: "assets/pieces/white rook.svg",
+  [FENChar.WhiteQueen]: "assets/pieces/white queen.svg",
+  [FENChar.WhiteKing]: "assets/pieces/white king.svg",
+  [FENChar.BlackPawn]: "assets/pieces/black pawn.svg",
+  [FENChar.BlackKnight]: "assets/pieces/black knight.svg",
+  [FENChar.BlackBishop]: "assets/pieces/black bishop.svg",
+  [FENChar.BlackRook]: "assets/pieces/black rook.svg",
+  [FENChar.BlackQueen]: "assets/pieces/black queen.svg",
+  [FENChar.BlackKing]: "assets/pieces/black king.svg"
+}
+
+export type SafeSquares = Map<string, Coords[]>;
+
+export type LastMove = {
+  piece: Piece;
+  prevX: number;
+  prevY: number;
+  currX: number;
+  currY: number;
+}
+
+type KingChecked = {
+  isInCheck: true;
+  x: number;
+  y: number;
+
+}
+
+type KingNotChecked = {
+  isInCheck: false;
+}
+
+export type CheckState = KingChecked|KingNotChecked;
