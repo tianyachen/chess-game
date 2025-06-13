add5139ffca4d141192450b7815377a239fa1f2b
Tianya Chen <tianyakc@gmail.com>
2025-06-12 21:48:42 -0400

    added routing to navigate between pvp mode and pvc mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
-import { ChessBoardComponent } from "./modules/chess-board/chess-board.component";
+import { ChessBoardComponent } from './modules/chess-board/chess-board.component';
+import { ComputerModeComponent } from './modules/computer-mode/computer-mode.component';
+import { NavMenuComponent } from './modules/nav-menu/nav-menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ChessBoardComponent],
+  imports: [NavMenuComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'chess-game';
